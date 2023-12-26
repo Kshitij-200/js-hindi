e6a379bf2610b386f25d78ddd4b0f37c147ff287
Kshitij Pratap Tomer <93885290+Kshitij-200@users.noreply.github.com>
2023-12-26 12:27:54 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "kshitijpratap2004@gmail.com"
+var accountPasswor = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 2 not allowed to change
+console.log(accountID);
+accountEmail = "xyz@gmail.com"
+accountPasswor = "123456"
+accountCity = "up"
+
+console.table([accountID,accountEmail,accountPasswor,accountCity,accountState])
