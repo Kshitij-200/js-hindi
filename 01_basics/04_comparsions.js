2971951a34a8022e483503f0d9ce402ffbbabae9
Kshitij Pratap Tomer <93885290+Kshitij-200@users.noreply.github.com>
2023-12-26 13:09:33 +0000

    comparsions done

---
+++ 01_basics/04_comparsions.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0); // all comparsions from undefined is false only
+// === -> strictly check, also checks datatypes
+
+console.log("2" === 2);
